f9a58d7912c8519b8d9cf575cac9e6d475ba835a
Juan Hebert <juan@skupee.online>
2023-07-10 20:52:32 -0600

    added clock

--- src/lib/components/DatePicker.svelte
+++ src/lib/components/DatePicker.svelte
@@ -4,16 +4,30 @@
 	import Input from './Input.svelte'
 	import Icon from 'svelte-icons-pack'
 	import AiOutlineCalendar from 'svelte-icons-pack/ai/AiOutlineCalendar'
+	import AiOutlineFieldTime from 'svelte-icons-pack/ai/AiOutlineFieldTime'
+
 	import Popup from './Popup.svelte'
+	import MainButton from './MainButton.svelte'
 	let date = new Date()
 	let locale = localeFromDateFnsLocale(hy)
 	let inputRef: HTMLElement
 	let popupRef: Popup
+	let currentStep = 0
+	let currentDate: Date = new Date()
+	let currentTime = {
+		hours: 0,
+		minutes: 0,
+		am: true
+	}
+	let formattedDate: string = ''
 	function handleFocus(ev) {
 		popupRef.show()
 	}
 	$: {
-		console.log(inputRef)
+		formattedDate = new Intl.DateTimeFormat('en-US', {
+			day: '2-digit',
+			month: 'short'
+		}).format(currentDate)
 	}
 </script>
 
@@ -21,4 +35,113 @@
 	<Icon slot="trailing" src={AiOutlineCalendar} color="currentColor" />
 </Input>
 
-<Popup bind:this={popupRef} trigger={inputRef} />
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<Popup bind:this={popupRef} trigger={inputRef}>
+	<div class="date-container">
+		<div class="tabs">
+			<div class="date" on:click={() => (currentStep = 0)} class:selected={currentStep == 0}>
+				{formattedDate}
+			</div>
+			<div class="time">
+				<span on:click={() => (currentStep = 1)} class:selected={currentStep == 1}>
+					{currentTime.hours.toString().padStart(2, '0')}
+				</span>
+				<span>:</span>
+				<span on:click={() => (currentStep = 2)} class:selected={currentStep == 2}>
+					{currentTime.minutes.toString().padStart(2, '0')}
+				</span>
+				<span class="meridian">
+					<div on:click={() => (currentTime.am = true)} class:selected={currentTime.am}>
+						AM
+					</div>
+					<div on:click={() => (currentTime.am = false)} class:selected={!currentTime.am}>
+						PM
+					</div>
+				</span>
+			</div>
+		</div>
+		<div class="tab-content">
+			{#if currentStep == 0}
+				<DatePicker bind:value={currentDate} on:select={(ev) => (currentStep = 1)} />
+			{:else if currentStep > 0}
+				<div class="clock" />
+			{/if}
+		</div>
+		<div class="actions">
+			<MainButton>Cancel</MainButton>
+			<MainButton>Ok</MainButton>
+		</div>
+	</div>
+</Popup>
+
+<style lang="scss">
+	.date-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-flow: column;
+		width: 500px;
+		height: 400px;
+		.tabs {
+			width: 100%;
+			background-color: var(--accent);
+			color: #ffffffbf;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			gap: 1rem;
+			padding: 0.6em;
+			font-size: 1em;
+			.selected {
+				color: #fff;
+			}
+			.date {
+				font-size: 3em;
+				cursor: pointer;
+				transition: color 0.3s ease-in-out;
+			}
+			.time {
+				display: flex;
+				gap: 0.2em;
+				font-size: 3em;
+				> span {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					cursor: pointer;
+
+					transition: color 0.3s ease-in-out;
+				}
+				.meridian {
+					display: flex;
+					flex-flow: column;
+					align-items: center;
+					font-size: 0.4em;
+					gap: 0.2em;
+					justify-content: center;
+					> div {
+						line-height: 0.8em;
+					}
+				}
+			}
+		}
+		.tab-content {
+			height: 100%;
+			display: flex;
+			align-items: center;
+		}
+		.actions {
+			height: 50px;
+			width: 100%;
+			display: flex;
+			gap: 0.5em;
+			padding: 0.5rem;
+		}
+	}
+	.clock {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		background-color: #00000020;
+	}
+</style>
--- src/lib/components/Popup.svelte
+++ src/lib/components/Popup.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+	import { fade, slide } from 'svelte/transition'
 	export let trigger: HTMLElement | null = null
 	let visible = false
 	export function show() {
@@ -7,8 +9,50 @@
 	export function hide() {
 		visible = false
 	}
+	onMount(() => {
+		const handleBackButton = (event) => {
+			event.preventDefault()
+		}
+		window.addEventListener('popstate', handleBackButton)
+		return () => {
+			window.removeEventListener('popstate', handleBackButton)
+		}
+	})
 </script>
 
-<div class="main-container">
-	<div />
-</div>
+{#if visible}
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class="main-container">
+		<div class="bg" transition:fade on:click|self={hide} />
+		<div transition:slide class="slot-container">
+			<slot />
+		</div>
+	</div>
+{/if}
+
+<style lang="scss">
+	.main-container {
+		position: fixed;
+		inset: 0;
+		z-index: 9999;
+		.bg {
+			background-color: #000000a0;
+			position: absolute;
+			inset: 0;
+		}
+		.slot-container {
+			background: var(--gd-page);
+			position: absolute;
+			overflow-y: auto;
+			bottom: 0;
+			left: 50%;
+			transform: translateX(-50%);
+			background-color: beige;
+			width: fit-content;
+			max-width: 640px;
+			max-height: 80%;
+			height: fit-content;
+			border-radius: 1em 1em 0 0;
+		}
+	}
+</style>
