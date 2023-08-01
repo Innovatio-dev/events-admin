c6d95e8b228679c56209c4e652289799a0cdab33
josejames <rodriguezjjaime@hotmail.com>
2023-07-31 18:44:32 -0600

    Fix: statuses updated for organizers

--- src/lib/server/constants/statuses.ts
+++ src/lib/server/constants/statuses.ts
@@ -1,20 +1,19 @@
 const organizerRequestStatuses = {
-    PENDING: 0,
-    APPROVED: 1,
-    DENIED: 2
+	PENDING: 0,
+	APPROVED: 1,
+	DENIED: 2
 }
 
 const eventStatuses = {
-    DRAFT: 0,
-    PUBLISHED: 1,
-    CANCELLED: 2
+	DRAFT: 0,
+	PUBLISHED: 1,
+	CANCELLED: 2
 }
 
 const organizerStatuses = {
-    INACTIVE: 0,
-    ACTIVE: 1,
-    SUSPENDED: 2
+	// INACTIVE: 0,
+	ACTIVE: 0,
+	SUSPENDED: 1
 }
-
 
 export { organizerRequestStatuses, eventStatuses, organizerStatuses }
