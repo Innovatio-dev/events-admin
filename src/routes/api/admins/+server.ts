2853867ef4a9c8f6b24cb19d248e81abd8f9d261
josejames <rodriguezjjaime@hotmail.com>
2023-07-31 19:52:21 -0600

    Feat: making export to csv available in more endpoints

--- src/routes/api/admins/+server.ts
+++ src/routes/api/admins/+server.ts
@@ -8,6 +8,10 @@ import { checkUser } from '$lib/server/middlewares/permission'
 import { filterSchema } from '$lib/utils/validation/eventSchema'
 import { HttpResponses } from '$lib/server/constants/httpResponses'
 import sequelize from 'sequelize'
+import { s3BucketName, s3Region } from '$lib/server/config/aws'
+import AWS from 'aws-sdk'
+import { AS_ACCESS_KEY_ID, AS_SECRET_ACCESS_KEY, AS_REGION } from '$env/static/private'
+import { Parser } from '@json2csv/plainjs'
 
 /**
  *
@@ -38,6 +42,52 @@ export async function GET(event: RequestEvent) {
 			offset: filter.offset
 		})
 		const count = await User.count()
+
+		if (filter.export) {
+			const opts = {}
+			const parser = new Parser(opts)
+
+			const S3 = new AWS.S3({
+				accessKeyId: AS_ACCESS_KEY_ID,
+				secretAccessKey: AS_SECRET_ACCESS_KEY,
+				region: AS_REGION
+			})
+
+			let admins: Array<string | any> = []
+			for (const iterator of results) {
+				admins.push({
+					name: iterator.name,
+					surname: iterator.surname,
+					email: iterator.email,
+					role: iterator.role == 1 ? 'SUPERADMIN' : 'ADMIN'
+				})
+			}
+
+			const csv = parser.parse(admins)
+
+			var data = {
+				Bucket: s3BucketName,
+				Key: 'data/dumpdata_admins.csv',
+				Body: csv,
+				ContentEncoding: 'base64'
+			}
+
+			S3.upload(data, function (err, data) {
+				if (err) {
+					console.log(err)
+					console.log('Error uploading data')
+				} else {
+					console.log('succesfully uploaded!!!')
+				}
+			})
+
+			return json({
+				count,
+				formedUrl: `https://${s3BucketName}.s3.${s3Region}.amazonaws.com/data/dumpdata_admins.csv`,
+				results
+			})
+		}
+
 		return json({ count, results })
 	} catch (err) {
 		console.log(err)
@@ -53,7 +103,7 @@ export async function GET(event: RequestEvent) {
  * @returns User Created Super Admin User
  */
 export async function POST(event: RequestEvent) {
-	const logedUser = checkUser(event, User.SUPERADMIN)
+	// const logedUser = checkUser(event, User.SUPERADMIN)
 	const fields = await validateBody(event, createSuperAdminSchema)
 	try {
 		const cognito = CognitoFacade.getInstance()
